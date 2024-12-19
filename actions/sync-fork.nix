5c5a75b37480ff62fb4d1b1a4758ffc7fb7c9bc3
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-12-19 15:15:53 +0000

    actions/sync-fork: init

---
+++ actions/sync-fork.nix
@@ -0,0 +1,24 @@
+{
+  name = "Sync fork with upstream";
+  on = {
+    schedule = [ { cron = "0 */6 * * *"; } ];
+    workflow_dispatch = null;
+  };
+
+  jobs.sync-fork = {
+    strategy.matrix.branch = [
+      "master"
+      "staging"
+    ];
+    runs-on = "ubuntu-latest";
+    steps = [
+      {
+        uses = "thiagokokada/merge-upstream@v1.0.2";
+        "with" = {
+          branch = ''''${{ matrix.branch }}'';
+          token = ''''${{ secrets.PAT_TOKEN }}'';
+        };
+      }
+    ];
+  };
+}
